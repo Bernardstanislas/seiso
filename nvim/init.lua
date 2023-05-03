50702c846cef90d935e30d5efc1b63b26a7f7553
Stanislas Bernard <bernardstanislas@gmail.com>
2023-05-03 11:04:33 +0200

    :wrench: Basic config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,10 +1,62 @@
+local opt = vim.opt
+local g = vim.g
+
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
+------------------------------- options ----------------------------
+
+-- disable Netrw
+g.loaded_netrw = 1
+g.loaded_netrwPlugin = 1
+
+-- update leader
+g.mapleader = " "
+
+-- disable leader timeout
+vim.api.nvim_set_option('timeout', false)
+
+-- set termguicolors to enable highlight groups
+opt.termguicolors = true
+
+opt.laststatus = 3 -- global statusline
+opt.showmode = false
+
+opt.clipboard = "unnamedplus"
+opt.cursorline = true
+
+-- Indenting
+opt.expandtab = true
+opt.shiftwidth = 2
+opt.smartindent = true
+opt.tabstop = 2
+opt.softtabstop = 2
+
+opt.fillchars = { eob = " " }
+opt.ignorecase = true
+opt.smartcase = true
+opt.mouse = "a"
+
+-- Numbers
+opt.number = true
+opt.numberwidth = 2
+opt.ruler = false
+
+opt.splitbelow = true
+opt.splitright = true
+opt.termguicolors = true
+opt.timeoutlen = 400
+opt.undofile = true
+
+-- interval for writing swap file to disk, also used by gitsigns
+opt.updatetime = 250
+
+----------------------------- Lazy vim -----------------------------
 require "init.lazy"
 
+------------------------------ plugins -----------------------------
 local plugins = require "plugins"
 require("lazy").setup(plugins)
 
--- set termguicolors to enable highlight groups
-vim.opt.termguicolors = true
+------------------------------ mappings ----------------------------
+require "mappings"
