7bb6ca4509c3666905d1d696d7211f316bf00809
Stanislas Bernard <bernardstanislas@gmail.com>
2023-05-03 08:09:16 +0200

    :recycle: Install neovim from scratch

--- nvim/lua/custom/configs/prettier.lua
+++
@@ -1,9 +0,0 @@
-local prettier = require("prettier")
-
-prettier.setup({
-  cli_options = {
-    -- https://prettier.io/docs/en/cli.html#--config-precedence
-    config_precedence = "prefer-file", -- or "cli-override" or "file-override"
-  },
-})
-
---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,6 @@
+local plugins = {
+  "kwsp/halcyon-neovim",
+  "nvim-tree/nvim-web-devicons"
+}
+
+return plugins
