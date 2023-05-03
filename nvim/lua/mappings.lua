243bd9616b00b691e006c33a9ed129e626e66333
Stanislas Bernard <bernardstanislas@gmail.com>
2023-05-03 14:24:25 +0200

    :recycle: Move keymaps to plugin init functions

--- nvim/lua/mappings.lua
+++
@@ -1,6 +0,0 @@
------------- tmux navigator
-vim.keymap.set({"n", "i"}, "<C-h>", ":TmuxNavigateLeft<CR>")
-vim.keymap.set({"n", "i"}, "<C-j>", ":TmuxNavigateDown<CR>")
-vim.keymap.set({"n", "i"}, "<C-k>", ":TmuxNavigateUp<CR>")
-vim.keymap.set({"n", "i"}, "<C-l>", ":TmuxNavigateRight<CR>")
-
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -16,8 +16,24 @@ local plugins = {
       vim.keymap.set("n", "<C-b>", ":Neotree toggle<CR>")
     end
   },
-  "christoomey/vim-tmux-navigator",
-  "TimUntersberger/neogit"
+  {
+    "christoomey/vim-tmux-navigator",
+    init = function ()
+      vim.keymap.set({"n", "i"}, "<C-h>", ":TmuxNavigateLeft<CR>")
+      vim.keymap.set({"n", "i"}, "<C-j>", ":TmuxNavigateDown<CR>")
+      vim.keymap.set({"n", "i"}, "<C-k>", ":TmuxNavigateUp<CR>")
+      vim.keymap.set({"n", "i"}, "<C-l>", ":TmuxNavigateRight<CR>")
+    end
+  },
+  {
+    "TimUntersberger/neogit",
+    init = function ()
+      vim.keymap.set({"n", "i"}, "<Leader>gg", ":Neogit<CR>")
+    end,
+    opts = {
+      kind = "split"
+    }
+  }
 }
 
 return plugins
