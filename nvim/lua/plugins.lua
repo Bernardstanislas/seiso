1421a449089a6259c00c5d0964a3757738c230c8
Stanislas Bernard <bernardstanislas@gmail.com>
2023-05-03 14:36:48 +0200

    :heavy_plus_sign: Add telescope

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -33,6 +33,21 @@ local plugins = {
     opts = {
       kind = "split"
     }
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    config = function()
+      local telescope = require "telescope"
+      local builtin = require "telescope.builtin"
+      telescope.setup()
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+      vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+      vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+      vim.keymap.set('n', '<leader>fk', builtin.keymaps, {})
+    end
   }
 }
 
