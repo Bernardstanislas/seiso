34be7ae434bd2de27cf855d6de7e287ddf39f107
Stanislas Bernard <bernardstanislas@gmail.com>
2023-05-03 16:21:38 +0200

    :zap: Lazy load plugins with keymaps

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -7,23 +7,23 @@ local plugins = {
   },
   {
     "nvim-neo-tree/neo-tree.nvim",
+    keys = {
+      { "<C-b>",  "<cmd>Neotree toggle<cr>", silent = true, desc = "toggle neotree"},
+    },
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-tree/nvim-web-devicons",
       "MunifTanjim/nui.nvim",
-    },
-    config = function ()
-      vim.keymap.set("n", "<C-b>", ":Neotree toggle<CR>", {silent = true})
-    end
+    }
   },
   {
     "christoomey/vim-tmux-navigator",
-    init = function ()
-      vim.keymap.set({"n", "i"}, "<C-h>", ":TmuxNavigateLeft<CR>")
-      vim.keymap.set({"n", "i"}, "<C-j>", ":TmuxNavigateDown<CR>")
-      vim.keymap.set({"n", "i"}, "<C-k>", ":TmuxNavigateUp<CR>")
-      vim.keymap.set({"n", "i"}, "<C-l>", ":TmuxNavigateRight<CR>")
-    end
+    keys = {
+      { "<C-h>", "<cmd>TmuxNavigateLeft<cr>", desc = "window left"},
+      { "<C-j>", "<cmd>TmuxNavigateDown<cr>", desc = "window down"},
+      { "<C-k>", "<cmd>TmuxNavigateUp<cr>", desc = "window up"},
+      { "<C-l>", "<cmd>TmuxNavigateRight<cr>", desc = "window right"},
+    },
   },
   {
     "TimUntersberger/neogit",
@@ -39,16 +39,13 @@ local plugins = {
     dependencies = {
       "nvim-lua/plenary.nvim",
     },
-    config = function()
-      local telescope = require "telescope"
-      local builtin = require "telescope.builtin"
-      telescope.setup()
-      vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-      vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-      vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-      vim.keymap.set('n', '<leader>fk', builtin.keymaps, {})
-    end
-  }
+    keys = {
+      { "<leader>ff", "<cmd>Telescope find_files hidden=true prompt_position=true<cr>", desc = "find files" },
+      { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "find buffers" },
+      { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "find help" },
+      { "<leader>fk", "<cmd>Telescope keymaps<cr>", desc = "find keymaps" },
+    },
+  },
 }
 
 return plugins
