2c6b970b82f8c56b5301b8576c6c1b8a444b3190
Stanislas Bernard <bernardstanislas@gmail.com>
2023-05-06 17:32:10 +0200

    :tada: Install lazy starter

--- nvim/lua/mappings.lua
+++
@@ -1,2 +0,0 @@
-vim.keymap.set("n", "<tab>", "<cmd>b#<cr>")
-
---
+++ nvim/lua/plugins/theme.lua
@@ -0,0 +1,14 @@
+return {
+	-- add halcyon
+	{
+		"kwsp/halcyon-neovim",
+	},
+
+	-- Configure LazyVim to load halcyon
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "halcyon",
+		},
+	},
+}
