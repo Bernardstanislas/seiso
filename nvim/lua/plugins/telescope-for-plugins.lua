674661277ec4650e3aa067691b3ec205eb17d4e0
Stanislas Bernard <bernardstanislas@gmail.com>
2023-05-21 23:08:40 +0200

    :recycle: Change fp to find projects

--- nvim/lua/plugins/telescope-for-plugins.lua
+++ nvim/lua/plugins/telescope-for-plugins.lua
@@ -6,8 +6,8 @@ return {
     -- stylua: ignore
     {
       "<leader>fp",
-      function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
-      desc = "Find Plugin File",
+      function() require("telescope").extensions.projects.projects({}) end,
+      desc = "Find projects",
     },
 	},
 	-- change some options
